7218892f46bee24d6ad3a7abc2b2324dce881a75
EveSunMaple <evesunmaple@outlook.com>
2023-05-16 22:58:03 +0800

    vault backup: 2023-05-16 22:58:03

--- source/js/title.js
+++ source/js/title.js
@@ -4,11 +4,11 @@ var titleTime;
 document.addEventListener('visibilitychange', function () {
   if (document.hidden) {
     //离开当前页面时标签显示内容
-    document.title = '👀跑哪里去了~';
+    document.title = '小息片刻~';
     clearTimeout(titleTime);
   } else {
     //返回当前页面时标签显示内容
-    document.title = '🐖抓到你啦～';
+    document.title = '欢迎回来~';
     //两秒后变回正常标题
     titleTime = setTimeout(function () {
       document.title = OriginTitile;
