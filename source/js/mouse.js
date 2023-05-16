171720df1ad8309e068bb915a581eaad2f0fd7c0
EveSunMaple <evesunmaple@outlook.com>
2023-05-16 22:28:23 +0800

    vault backup: 2023-05-16 22:28:23

--- source/js/mouse.js
+++ source/js/mouse.js
@@ -39,7 +39,7 @@ class Cursor {
                 this.pt.push(el[i].outerHTML);
 
         document.body.appendChild((this.scr = document.createElement("style")));
-        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='.5'/></svg>") 4 4, auto}`;
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='1.0' fill='rgb(57, 197, 187)'/></svg>") 4 4, auto}`;
     }
 
     refresh() {
