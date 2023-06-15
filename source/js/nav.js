a400eba8e692e9019051cab9832918703b48f2ea
EveSunMaple <evesunmaple@outlook.com>
2023-06-15 19:43:23 +0800

    vault backup: 2023-06-15 19:43:23

--- source/js/nav.js
+++ source/js/nav.js
@@ -24,3 +24,24 @@ function scrollToTop() {
     document.getElementById("name-container").setAttribute("style", "display:none");
     btf.scrollToDest(0, 500);
 }
+function catalogActive () {
+    let $list = document.getElementById('catalog-list')
+    if ($list) {
+      // 鼠标滚轮滚动
+      $list.addEventListener('mousewheel', function (e) {
+        // 计算鼠标滚轮滚动的距离
+        $list.scrollLeft -= e.wheelDelta / 2
+        // 阻止浏览器默认方法
+        e.preventDefault()
+      }, false)
+  
+      // 高亮当前页面对应的分类或标签
+      let path = decodeURIComponent(window.location.pathname).replace(/page\/[0-9]+\//g, '')
+      let $catalog = document.getElementById(path)
+      $catalog?.classList.add('selected')
+  
+      // 滚动当前页面对应的分类或标签到中部
+      $list.scrollLeft = ($catalog.offsetLeft - $list.offsetLeft) - ($list.offsetWidth - $catalog.offsetWidth) / 2
+    }
+  }
+  catalogActive()
